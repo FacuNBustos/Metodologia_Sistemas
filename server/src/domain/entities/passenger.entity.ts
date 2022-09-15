3e9bda0678d710c530b94633fc9ce95dcda277a9
Lisandro Bonino <lisandrobonino4@protonmail.com>
2022-09-14 23:21:22 -0300

    Add passenger entity

---
+++ server/src/domain/entities/passenger.entity.ts
@@ -0,0 +1,72 @@
+import { v4 } from 'uuid';
+
+export class Passenger {
+
+    private id: string;
+    private fullName: string;
+    private email: string;
+    private identityCard: string;
+
+    constructor(
+        id: string,
+        fullName: string,
+        email: string,
+        identityCard: string,
+    ) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.identityCard = identityCard;
+    }
+
+    static create(fullName: string, email: string, identityCard: string): Passenger {
+        const id = v4();
+        const passenger = new Passenger(id, fullName, email, identityCard);
+
+        return passenger;
+    }
+
+    static fromPrimitives(primitives: any): Passenger {
+        const passenger = new Passenger(
+            primitives.id,
+            primitives.fullName,
+            primitives.email,
+            primitives.identityCard
+        );
+
+        return passenger;
+    }
+
+    changeEmail(email: string): void {
+        this.email = email;
+    }
+
+    changeFullName(fullName: string): void {
+        this.fullName = fullName;
+    }
+
+    getId(): string {
+        return this.id;
+    }
+
+    getFullName(): string {
+        return this.fullName;
+    }
+
+    getEmail(): string {
+        return this.email;
+    }
+
+    getIdentityCard(): string {
+        return this.identityCard;
+    }
+
+    toPrimitives(): any {
+        return {
+            id: this.id,
+            fullName: this.fullName,
+            email: this.email,
+            identityCard: this.identityCard,
+        }
+    }
+}
