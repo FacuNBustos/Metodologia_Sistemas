6934255f09340a90dc86428aac0d74fa3bf1bd21
Paolo Gamarra <paologamarra10@gmail.com>
2022-10-13 15:09:36 -0300

    create findOneByIdAccommodationCommand and bug fixes

--- server/src/infraestructure/repositories/accommodation.repository.ts
+++ server/src/infraestructure/repositories/accommodation.repository.ts
@@ -1,4 +1,6 @@
 import { Accommodation } from "../../domain/entities/accommodation.entity";
+import { findOneByIdAccommodationCommand } from "../../application/commands/accommodations/findOneById.accommodation.command";
+import { findOneByNameAccommodationCommand } from "../../application/commands/accommodations/findOneByName.accommodation.command";
 
 class AccommodationRepository {
   
@@ -20,7 +22,7 @@ class AccommodationRepository {
     this.accommodations.push(accommodation);
   }
 
-  async findOneById(command: findByIdAccommodationCommand): Promise<Accommodation | null> {
+  async findOneById(command: findOneByIdAccommodationCommand): Promise<Accommodation | null> {
     const accommodation = this.accommodations.find(function(a) {
       return a.getId() === command.getId();
     });
@@ -29,7 +31,6 @@ class AccommodationRepository {
   };
   
   async findOneByName(command: findOneByNameAccommodationCommand): Promise<Accommodation | null> {
-    //const accommodation = this.accommodations.includes(command); 
     const accommodation = this.accommodations.find(function(a) {
       return a.getName() === command.getName();
     });
