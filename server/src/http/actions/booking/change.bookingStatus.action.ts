8f15b65e1aa167544f13ab2ee63085a119768ca9
Matias Luengo <matiasluengoo95@gmail.com>
2022-10-13 16:16:45 -0300

    Create Update Booking status action

---
+++ server/src/http/actions/booking/change.bookingStatus.action.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { UpdateBookingStatusCommand } from "../../../application/commands/bookings/update.bookingStatus.command";
+import updateBookingStatusHandler from '../../../application/handlers/booking/update.bookingStatus.handler';
+
+class UpdateBookingStatusAction {
+  async run(req: Request, res: Response) {
+
+    try {
+      const command = new UpdateBookingStatusCommand(
+        
+        req.body.status
+      );
+
+      try {
+        
+        await updateBookingStatusHandler.execute(command);
+
+      } catch ( error:any ) {
+        return res.status(404).json({message: error.message});
+      }
+
+      return res.status(200).json({message: "Booking Status updated"});
+    } catch ( error:any ) {
+      return res.status(400).json({message: error.message});
+    }
+  }
+}
+
+export default new UpdateBookingStatusAction();
