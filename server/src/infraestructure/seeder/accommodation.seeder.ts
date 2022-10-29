a98368038f7a8e1ce0c4c798a219f3c39b21ed1c
Facundo <Facu.Bustos00@hotmail.com>
2022-10-29 19:41:50 -0300

    change values of seeder

--- server/src/infraestructure/seeder/accommodation.seeder.ts
+++ server/src/infraestructure/seeder/accommodation.seeder.ts
@@ -5,9 +5,9 @@ import accommodationRepository from "../repositories/accommodation.repository";
 class Seeder {
     private accommodations: Array<Accommodation> = [];
     builder(){
-        this.accommodations.push(new Accommodation('1','Matias',5500));
-        this.accommodations.push(new Accommodation('2','Tomas', 9000));
-        this.accommodations.push(new Accommodation('3','Francisco', 7890));
+        this.accommodations.push(new Accommodation('b530e645-e97d-4368-9e3f-167ab25f99ee','Hotel Menem',5500));
+        this.accommodations.push(new Accommodation('52e3e369-d4a1-4fc3-b5b7-873d2254ea1a','Hotel Sarmiento', 9000));
+        this.accommodations.push(new Accommodation('b5564183-be7a-42fb-aca5-7d22351fa57c','Hotel Tu Vieja', 7890));
     }
 
     public async generate(): Promise<void> {
